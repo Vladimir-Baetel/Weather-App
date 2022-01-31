23f8d54cc64e47881a2a77b7bf89d090325e56b9
Vladimir-Baetel <vladimir.baetel@gmail.com>
2022-01-31 12:58:55 +0200

    refactoring of entire JS

--- script.js
+++ script.js
@@ -1,109 +1,95 @@
+const infoDomElement = document.querySelector(".info");
+const input = document.querySelector(".search > input");
 let city = "";
 
-// function getLocation() {
-//   const url = "http://extreme-ip-lookup.com/json/?key=XyjcKh4Jb6gAcMbyqPRc";
+infoDomElement.innerHTML = "<h1>Loading... 😎</h1>";
 
-//   fetch(url)
-//     .then((response) => {
-//       if (!response.ok) {
-//         city = "Bucharest";
-//       }
-//       return response.json();
-//     })
-//     .catch(() => {
-//       city = "Bucharest";
-//     })
-//     .then(() => {
-//       city = data["city"];
-//     });
-// }
-// getLocation();
-
-async function getLoc() {
+async function getLocation() {
   try {
     const url = "https://extreme-ip-lookup.com/json/?key=XyjcKh4Jb6gAcMbyqPRc";
     const res = await fetch(url);
     const jsonRes = await res.json();
-    city = jsonRes.city;
+    city = await jsonRes.city;
   } catch (e) {
     city = "Bucharest";
-    console.log("Location could not be acessed, default is Bucharest");
+    console.log(
+      "Location could not be acessed automatically, default is Bucharest"
+    );
   }
 }
-getLoc();
+getLocation();
 
 setTimeout(function () {
   getAPI();
-}, 100);
+}, 1000);
 
-const input = document.querySelector(".search > input");
-const loc = document.querySelector(".location > h2");
-const date = document.querySelector(".date > p");
-const time = document.querySelector(".time > p > span");
-const temp = document.querySelector(".temp");
-const cond = document.querySelector(".cond");
-const icon = document.querySelector(".icon");
-const feel = document.querySelector(".feel");
-
-input.addEventListener("keydown", function (listen) {
-  if (listen.code === "Enter") {
+input.addEventListener("keydown", function (e) {
+  if (e.code === "Enter") {
     city = input.value;
     city = city.charAt(0).toUpperCase() + city.slice(1).toLowerCase();
-    getAPI();
+
+    infoDomElement.innerHTML = "<h1>Loading</h1>";
+
+    setTimeout(function () {
+      getAPI();
+    }, 1000);
   }
 });
 
-function getAPI() {
-  const url =
-    "https://api.weatherapi.com/v1/current.json?key=d7db22f333fc4e6aaf3110311222601&q=" +
-    city +
-    "&aqi=no";
-  fetch(url)
-    .then((response) => {
-      if (!response.ok) {
-        loc.innerHTML = "😞 Try Again";
-        date.innerHTML = "";
-        temp.innerHTML = "0";
-        cond.innerHTML = "";
-        icon.src = "";
-        feel.innerHTML = "0";
-      }
-      return response.json();
-    })
-    .then((data) => {
-      let locData = data["location"]["name"];
-      let dateData = data["location"]["localtime"];
-      let tempData = data["current"]["temp_c"];
-      tempData = Math.floor(tempData);
-      let condData = data["current"]["condition"]["text"];
-      let iconData = data["current"]["condition"]["icon"];
-      let feelData = data["current"]["feelslike_c"];
+async function getAPI() {
+  try {
+    const url =
+      "https://api.weatherapi.com/v1/current.json?key=d7db22f333fc4e6aaf3110311222601&q=" +
+      city +
+      "&aqi=no";
+    const res = await fetch(url);
+    const jsonRes = await res.json();
 
-      let dateOptionsD = {
-        weekday: "long",
-        year: "numeric",
-        month: "long",
-        day: "numeric",
-      };
-      let dateOptionsT = {
-        hour: "numeric",
-        minute: "numeric",
-        second: "numeric",
-      };
-      dateDataD = new Date(dateData).toLocaleString("en-GB", dateOptionsD);
-      dateDataT = new Date(dateData).toLocaleString("en-GB", dateOptionsT);
+    let location = await jsonRes.location.name;
+    let datetime = await jsonRes.location.localtime;
+    const optionsDate = {
+      weekday: "long",
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    };
+    const optionsTime = {
+      hour: "numeric",
+      minute: "numeric",
+      second: "numeric",
+    };
+    let date = new Date(datetime).toLocaleString("en-GB", optionsDate);
+    let time = new Date(datetime).toLocaleString("en-GB", optionsTime);
+    let icon = await jsonRes.current.condition.icon;
+    let condition = await jsonRes.current.condition.text;
+    let temperature = await jsonRes.current.temp_c;
+    temperature = Math.floor(temperature);
+    let feelslike = await jsonRes.current.feelslike_c;
+    feelslike = Math.floor(temperature);
 
-      loc.innerHTML = locData;
-      date.innerHTML = dateDataD;
-      time.innerHTML = dateDataT;
-      temp.innerHTML = tempData;
-      cond.innerHTML = condData;
-      icon.src = iconData;
-      feel.innerHTML = feelData;
-    });
+    infoDomElement.innerHTML =
+      "<div class='location'><h2>" +
+      location +
+      "</h2></div>" +
+      "<div class='date'><p>" +
+      date +
+      "</p></div>" +
+      "<div class='time'><p>" +
+      time +
+      "</p></div>" +
+      "<div class='condition flex'><img src='" +
+      icon +
+      "'/><p>" +
+      condition +
+      "</p></div>" +
+      "<div class='temperature flex'><h3><span>" +
+      temperature +
+      "</span><sup>°</sup></h3><h3>C</h3></div>" +
+      "<div class='feels-like'><p>Feels like <span>" +
+      feelslike +
+      "</span><sup>°</sup>C</p></div>";
+  } catch (e) {
+    infoDomElement.innerHTML = "<h1>Location not found 😞</h1>";
+    console.log("Try searching again and pressing enter");
+  }
 }
-
-// TO DO:
-//  1 - Github
-//  2 - HTML dinamic din JS + remove temp, feel like, date, time, etc. when error
-//  3 - Cookies?
